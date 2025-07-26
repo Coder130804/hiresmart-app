3d2e50438d3fa8cb49d601a5406b86024d664a0b
Coder130804 <arpitasom157@yahoo.com>
2025-07-26 20:12:43 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // style reuse
+import './navbar.css'; // style reuse
 
 const Navbar = () => {
   return (
